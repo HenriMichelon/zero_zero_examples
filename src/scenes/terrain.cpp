1585e9ab11cb672253e6be355219c8cf72cf91ad
HenriMichelon <148531229+HenriMichelon@users.noreply.github.com>
2024-06-26 16:18:52 +0200

    Fixed shadow map memory leak

--- src/scenes/terrain.cpp
+++ src/scenes/terrain.cpp
@@ -9,9 +9,6 @@ void TerrainScene::onReady() {
     setProcessMode(PROCESS_MODE_ALWAYS);
     log("Loading scene");
     Loader::addSceneFromFile(this, "res/scenes/scene1.json");
-    auto player = getNode("game/player");
-    log(to_string(player->getPosition()));
-    log(to_string(player->getPositionGlobal()));
     auto usage = (app().getVideoMemoryUsage() / static_cast<float>(app().getDedicatedVideoMemory())) * 100.0f;
     log("VRAM usage", to_string(usage)+"%");
     printTree();
